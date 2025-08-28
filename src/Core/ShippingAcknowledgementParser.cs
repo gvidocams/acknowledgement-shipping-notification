f1ce2bda14d7b63de814c8b9700ad3f8842be79b
gvidocams <gaca6817@gmai.com>
2025-08-28 16:03:04 +0300

    Added initial shipping acknowledgement parser

---
+++ src/Core/ShippingAcknowledgementParser.cs
@@ -0,0 +1,44 @@
+using System.Threading.Channels;
+using Core.Converters;
+using Core.Models;
+
+namespace Core;
+
+public class ShippingAcknowledgementParser : IShippingAcknowledgementParser
+{
+    private const string BoxIdentifier = "HDR";
+    private const string ProductIdentifier = "LINE";
+    
+    public async Task ParseShippingAcknowledgementNotification(ChannelWriter<Box> writer, string filePath)
+    {
+        using var reader = new StreamReader(filePath);
+        Box? box = null;
+
+        while (await reader.ReadLineAsync() is { } line && await writer.WaitToWriteAsync())
+        {
+            if (line.StartsWith(BoxIdentifier))
+            {
+                if (box is not null)
+                {
+                    await writer.WriteAsync(box);
+                }
+
+                // TODO Create error handling if couldn't parse the box
+                box = BoxConverter.ToBox(line);
+            }
+            else if (line.StartsWith(ProductIdentifier))
+            {
+                // TODO Create error handling for cases when a line has been found before a box
+                // TODO Create error handling if couldn't parse the line
+                box?.Contents.Add(ContentConverter.ToContent(line));
+            }
+        }
+
+        if (box is not null && await writer.WaitToWriteAsync())
+        {
+            await writer.WriteAsync(box);
+        }
+
+        writer.Complete();
+    }
+}
---
+++ tests/Core.Tests/ShippingAcknowledgementParserTests.cs
@@ -0,0 +1,73 @@
+using System.Threading.Channels;
+using Core.Models;
+using Shouldly;
+
+namespace Core.Tests;
+
+public class ShippingAcknowledgementParserTests
+{
+    private readonly ShippingAcknowledgementParser _shippingAcknowledgementParser = new();
+
+    [Fact]
+    public async Task ParseShippingAcknowledgementNotification_WhenParsingFinished_ChannelShouldBeCompleted()
+    {
+        var channel = Channel.CreateUnbounded<Box>();
+
+        await _shippingAcknowledgementParser.ParseShippingAcknowledgementNotification(channel.Writer,
+            "./Data/multiple-boxes.txt");
+
+        channel.Writer.TryComplete().ShouldBeFalse();
+    }
+
+    [Fact]
+    public async Task ParseShippingAcknowledgementNotification_WhenParsingStarted_ChannelShouldBeCompleted()
+    {
+        var channel = Channel.CreateUnbounded<Box>();
+
+        await _shippingAcknowledgementParser.ParseShippingAcknowledgementNotification(channel.Writer,
+            "./Data/multiple-boxes.txt");
+
+        channel.Reader.Count.ShouldBe(3);
+
+        var expectedBoxes = new List<Box>();
+
+        await foreach (var box in channel.Reader.ReadAllAsync())
+        {
+            expectedBoxes.Add(box);
+        }
+
+        expectedBoxes.ShouldBeEquivalentTo(new List<Box>
+        {
+            new()
+            {
+                SupplierIdentifier = "TRSP117",
+                Identifier = "6874453I",
+                Contents =
+                [
+                    new Content { PoNumber = "P000001661", Isbn = "9781473663800", Quantity = 12 },
+                    new Content { PoNumber = "P000001661", Isbn = "9781473667273", Quantity = 2 },
+                    new Content { PoNumber = "P000001661", Isbn = "9781473665798", Quantity = 1 }
+                ]
+            },
+            new()
+            {
+                SupplierIdentifier = "TRSP117",
+                Identifier = "6874454I",
+                Contents =
+                [
+                    new Content { PoNumber = "G000009810", Isbn = "9781473669987", Quantity = 1 },
+                    new Content { PoNumber = "G000009810", Isbn = "9781473661905", Quantity = 1 }
+                ]
+            },
+            new()
+            {
+                SupplierIdentifier = "TRSP117",
+                Identifier = "6874473I",
+                Contents =
+                [
+                    new Content { PoNumber = "G000009809", Isbn = "9781473676978", Quantity = 1 },
+                ]
+            }
+        });
+    }
+}
