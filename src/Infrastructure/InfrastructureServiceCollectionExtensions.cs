25caa658dc8d0de48ea104ebaf1df96bb87f4fdb
gvidocams <gaca6817@gmai.com>
2025-08-29 19:36:02 +0300

    Updated infrastructure dependency injection

---
+++ src/Infrastructure/InfrastructureServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+using Core;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureServiceCollectionExtensions
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration) =>
+        services
+            .AddScoped<IShippingAcknowledgementRepository, ShippingAcknowledgementRepository>()
+            .AddScoped<IShippingAcknowledgementProvider, ShippingAcknowledgementProvider>()
+            .AddDbContext<ShippingAcknowledgementContext>(options => 
+                options.UseSqlite(configuration.GetConnectionString("ShippingAcknowledgementDatabase")));
+}
