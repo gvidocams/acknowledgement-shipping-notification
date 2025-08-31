f4faef1c89e6e8a2f34e74044cd9085c0e571ed2
gvidocams <gaca6817@gmai.com>
2025-08-31 20:08:20 +0300

    Added failure handling and logging for notification processing

--- src/Core/ShippingAcknowledgementScanner.cs
+++ src/Core/ShippingAcknowledgementScanner.cs
@@ -15,7 +15,19 @@ public class ShippingAcknowledgementScanner(
 
         foreach (var fileName in fileNames)
         {
-            await shippingAcknowledgementProcessor.ProcessShippingAcknowledgementNotification(fileName);
+            using var scope = logger.BeginScope("Acknowledgement notification: {FileName}", fileName);
+            try
+            {
+                logger.LogInformation("Processing shipping acknowledgement notification {FileName}", fileName);
+
+                await shippingAcknowledgementProcessor.ProcessShippingAcknowledgementNotification(fileName);
+
+                logger.LogInformation("Finished processing acknowledgement notification {FileName}", fileName);
+            }
+            catch (Exception exception)
+            {
+                logger.LogError(exception, "An error occured while processing acknowledgement notification {FileName}", fileName);
+            }
         }
     }
 }
